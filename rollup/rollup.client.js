983c4a6de1221cfd7fcee09c39dc032b0135702a
Tung Vu <me@tungv.com>
2016-04-24 12:14:56 +0700

    add build:watch script

--- rollup/rollup.client.js
+++ rollup/rollup.client.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'src/client.main.js',
-  format: 'cjs',
+  format: 'es6',
   plugins: [ json(), babel() ],
   dest: '_meteor/client/bundle.js'
 };
--- rollup/rollup.server.js
+++ rollup/rollup.server.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   entry: 'src/server.main.js',
-  format: 'cjs',
+  format: 'es6',
   plugins: [ json(), babel() ],
   dest: '_meteor/server/bundle.js'
 };
