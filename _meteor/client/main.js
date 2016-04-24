4c097418e9e59df465f181f1b5c3a336d1168ba3
Tung Vu <me@tungv.com>
2016-04-24 12:29:45 +0700

    WIP add react

--- _meteor/client/main.js
+++ _meteor/client/main.js
@@ -2,21 +2,10 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import ReactDOM from 'react-dom';
+import { ReactRoot } from './bundle';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.reactRoot.onCreated(() => {
+  const root = document.getElementById('reactRoot');
+  ReactDOM.render(<ReactRoot />, root);
 });
--- src/client.main.js
+++ src/client.main.js
@@ -2,3 +2,9 @@ import { version } from '../package.json';
 
 const message = `current version is ${version}`;
 export default () => console.log( message );
+
+import React from 'react';
+
+export const ReactRoot = () => (
+  <div>Hello</div>
+);
